2970f1812854ae25b5ade20243c978c60500dc6f
Xander Roper <roper.alexander@gmail.com>
2021-04-01 13:54:23 +0000

    Hard code quiz question servlet

--- src/main/java/com/google/sps/servlets/QuizQuestionsServlet.java
+++ src/main/java/com/google/sps/servlets/QuizQuestionsServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import com.google.gson.Gson;
 
@@ -15,16 +17,17 @@ public class QuizQuestionsServlet extends HttpServlet {
 
   static final long serialVersionUID = 0;
     class QuizQuestion {
-        String id;
-        String question;
-        ArrayList<String> answers;
-        String questionType;
+        HashMap<String, List<String>> quiz = new HashMap<>();
 
-        public QuizQuestion(String id, String question, ArrayList<String> answers, String questionType) {
-            this.id = id;
-            this.question = question;
-            this.answers = answers;
-            this.questionType = questionType;
+        public QuizQuestion() {
+            quiz.put("Which of these would you most like to do?", List.of("Go on a hike in nature", "Take a trip downtown", "Go to a museum", "Take a day for relaxation"));
+            quiz.put("What is your favorite food?", List.of("Fruit", "Burgers", "Wraps", "Steak"));
+            quiz.put("How active do you like to be?", List.of("Very much", "Much", "Not much", "Not at all"));
+            quiz.put("What do you value most?", List.of("Learning new thigs", "Physical activity", "Beautiful scenery", "New experiences"));
+            quiz.put("How long will your trip be?", List.of("2+ weeks", "1 week", "Less than a week", "One day"));
+            quiz.put("How much are you willing to spend?", List.of("$$$$", "$$$", "$$", "$"));
+            quiz.put("Will you bring children? If so, how many?", List.of("No", "1", "2-4", "4+"));
+
         }
     }
     @Override
@@ -34,7 +37,7 @@ public class QuizQuestionsServlet extends HttpServlet {
         a.add("Swim");
         a.add("Relax");
 
-        QuizQuestion quest = new QuizQuestion("0","What do you like to do?", a, "Introduction");
+        QuizQuestion quest = new QuizQuestion();
 
         // Convert the server stats to JSON
         String json = convertToJsonUsingGson(quest);
