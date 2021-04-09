aead40340c18253536e1a02c7a2051d73cc712ac
Manuela <manurod19@gmail.com>
2021-04-09 03:20:42 +0000

    load userAnswers from datastore into DestinationInfoServlet

--- src/main/java/com/google/sps/servlets/DestinationInfoServlet.java
+++ src/main/java/com/google/sps/servlets/DestinationInfoServlet.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.sps.data.Destination;
+import com.google.sps.data.UserAnswers;
+
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Query;
 import com.google.cloud.datastore.QueryResults;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.cloud.datastore.Value;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,42 +20,33 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+/**
+ * Handles requests sent to the /hello URL. Try running a server and navigating
+ * to /hello!
+ */
 @WebServlet("/getDestinationInfo")
 public class DestinationInfoServlet extends HttpServlet {
 
     static final long serialVersionUID = 0;
-    class Destination {
-        String name;
-        String overallExpense;
-        String type;
 
-        public Destination(String name, String overallExpense, String type) {
-            this.name = name;
-            this.overallExpense = overallExpense;
-            this.type = type;
-        }
-    }
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // get userAnswers from datastore and match them with a destination
-        /*
+
         Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        Query<Entity> query =
-        Query.newEntityQueryBuilder().setKind("Answer").setOrderBy(OrderBy.desc("timestamp")).build();
+        Query<Entity> query = Query.newEntityQueryBuilder().setKind("UserAnswers").build();
         QueryResults<Entity> results = datastore.run(query);
 
-        List<Answer> userAnswers = new ArrayList<>();
+        List<UserAnswers> userAnswers = new ArrayList<>();
         while (results.hasNext()) {
             Entity entity = results.next();
-            
-            long id = entity.getKey().getId();
-            String title = entity.getString("title");
 
-            Answer answer = new Answer(id, title);
+            long id = entity.getKey().getId();
+            List<Value<String>> allAnswers = entity.getList("AllAnswers");
+
+            UserAnswers answer = new UserAnswers(id, allAnswers);
             userAnswers.add(answer);
         }
-        */
 
         // Hard-coded destination info
         Destination newDestination = new Destination("Hawaii","$$$","beach");
